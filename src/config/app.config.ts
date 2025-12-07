ac057c2ceefad62f3b8543b0bfd070ce77fa8c5a
Karan Chauhan <karanchouhan211@gmail.com>
2025-12-07 16:21:29 +0100

    add docker secrets

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
-
+// need to change
 export default registerAs('app', () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     databaseUrl: process.env.DATABASE_URL,
